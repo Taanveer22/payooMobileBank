acef547336d9dd44266320d20a1f1d15b606665d
Taanveer22 <taanveer469@gmail.com>
2025-05-10 11:11:38 +0600

    alhamdulliah validate check done

---
+++ js/login.js
@@ -0,0 +1,22 @@
+console.log("connected");
+
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+    console.log("login btn clicked");
+
+    // Get values INSIDE the click handler
+    const mobNumber = document.getElementById("mob-number").value;
+    console.log(mobNumber);
+
+    const pinNumber = document.getElementById("pin-number").value;
+    console.log(pinNumber);
+
+    // validate user input
+    if (mobNumber === "01761248294" && pinNumber === "1234") {
+      console.log("congratulations! you are logged in");
+    } else {
+      alert("wrong input...");
+    }
+  });
