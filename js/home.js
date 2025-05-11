efc3ca6691816d4be4907a79f16d7dfe74590ea2
Taanveer22 <taanveer469@gmail.com>
2025-05-11 19:22:51 +0600

    alhamdulliah btn-add-money console check done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,19 @@
-console.log('home page connected');
+console.log("home page connected");
+
+// step-1 : add an eventListener to the btn-add-money inside the form 
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    console.log("btn-add-money clicked");
+    
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // step-2: get the amount of money to add
+    const amountInput = document.getElementById("amount-input").value;
+    console.log(amountInput);
+
+    // step-3: verify the pin number user provided
+    const pinInput = document.getElementById('pin-input').value;
+    console.log(pinInput);
+  });
